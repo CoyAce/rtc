09a8ecfe97c2211bea8ebd30c9860b06a2bdd9cb
CoyAce <AkeyCoy@gmail.com>
2025-12-06 15:21:02 +0800

    update main

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"log"
 
+	"github.com/CoyAce/live/rtc"
+)
+
+var (
+	address    = flag.String("a", "127.0.0.1:52000", "listen address")
+	serverMode = flag.Bool("s", true, "server mode")
+)
+
+func main() {
+	flag.Parse()
+	if *serverMode {
+	}
 }
