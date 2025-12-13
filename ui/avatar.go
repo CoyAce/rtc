858df3a43d53a0a07e95986572215cdf17273885
CoyAce <AkeyCoy@gmail.com>
2025-12-13 20:36:18 +0800

    refactoring avatar

--- ui/avatar.go
+++ ui/avatar.go
@@ -22,16 +22,12 @@ func (v *Avatar) Layout(gtx layout.Context) layout.Dimensions {
 	if v.Size == (image.Point{}) {
 		v.Size = image.Point{X: gtx.Dp(48), Y: gtx.Dp(48)}
 	}
-	gtx.Constraints.Min, gtx.Constraints.Max = v.Size, v.Size
-	var imgWidget widget.Image
 	if v.Image == nil {
 		v.Image = assets.AppIconImage
-		imgOps := paint.NewImageOp(v.Image)
-		imgWidget = widget.Image{Src: imgOps, Fit: widget.Fill, Position: layout.Center, Scale: 0}
-	} else {
-		imgOps := paint.NewImageOp(v.Image)
-		imgWidget = widget.Image{Src: imgOps, Fit: widget.Fill, Position: layout.Center, Scale: 0}
 	}
+	gtx.Constraints.Min, gtx.Constraints.Max = v.Size, v.Size
+	imgOps := paint.NewImageOp(v.Image)
+	imgWidget := widget.Image{Src: imgOps, Fit: widget.Fill, Position: layout.Center, Scale: 0}
 	defer clip.UniformRRect(image.Rectangle{
 		Max: image.Point{
 			X: gtx.Constraints.Max.X,
