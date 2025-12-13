35d1f579890a8df1213c73cb7c88f898553b5ec3
CoyAce <AkeyCoy@gmail.com>
2025-12-13 10:43:28 +0800

    default to client mode

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 
 var (
 	address         = flag.String("a", "127.0.0.1:52000", "listen address")
-	serverMode      = flag.Bool("s", true, "server mode")
+	serverMode      = flag.Bool("s", false, "server mode")
 	commandLineMode = flag.Bool("i", false, "server mode")
 )
 
