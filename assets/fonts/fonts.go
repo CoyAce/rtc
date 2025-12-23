14ecd4556e122267cd20e8347e98691bbbd92cb7
CoyAce <AkeyCoy@gmail.com>
2025-12-23 11:15:36 +0800

    delete not used fonts

--- assets/fonts/fonts.go
+++ assets/fonts/fonts.go
@@ -13,44 +13,17 @@ import (
 //go:embed NotoColorEmoji.ttf
 var notoColorEmoji []byte
 
-//go:embed NotoSansSC-Black.ttf
-var notoSansSCBlack []byte
-
 //go:embed NotoSansSC-Bold.ttf
 var notoSansSCBold []byte
 
-//go:embed NotoSansSC-Light.ttf
-var notoSansSCLight []byte
-
-//go:embed NotoSansSC-Medium.ttf
-var notoSansSCMedium []byte
-
 //go:embed NotoSansSC-Regular.ttf
 var notoSansSCRegular []byte
-
-//go:embed Roboto-Black.ttf
-var robotoBlack []byte
-
-//go:embed Roboto-Black-Italic.ttf
-var robotoBlackItalic []byte
 
 //go:embed Roboto-Bold.ttf
 var robotoBold []byte
 
 //go:embed Roboto-Bold-Italic.ttf
 var robotoBoldItalic []byte
-
-//go:embed Roboto-Light.ttf
-var robotoLight []byte
-
-//go:embed Roboto-Light-Italic.ttf
-var robotoLightItalic []byte
-
-//go:embed Roboto-Medium.ttf
-var robotoMedium []byte
-
-//go:embed Roboto-Medium-Italic.ttf
-var robotoMediumItalic []byte
 
 //go:embed Roboto-Regular.ttf
 var robotoRegular []byte
@@ -59,48 +32,27 @@ var robotoRegular []byte
 var robotoRegularItalic []byte
 
 var notoEmoji, _ = opentype.ParseCollection(notoColorEmoji)
-var black, _ = opentype.ParseCollection(robotoBlack)
-var blackItalic, _ = opentype.ParseCollection(robotoBlackItalic)
 var bold, _ = opentype.ParseCollection(robotoBold)
 var boldItalic, _ = opentype.ParseCollection(robotoBoldItalic)
-var light, _ = opentype.ParseCollection(robotoLight)
-var lightItalic, _ = opentype.ParseCollection(robotoLightItalic)
-var medium, _ = opentype.ParseCollection(robotoMedium)
-var mediumItalic, _ = opentype.ParseCollection(robotoMediumItalic)
 var regular, _ = opentype.ParseCollection(robotoRegular)
 var regularItalic, _ = opentype.ParseCollection(robotoRegularItalic)
-var scBlack, _ = opentype.ParseCollection(notoSansSCBlack)
 var scBold, _ = opentype.ParseCollection(notoSansSCBold)
-var scLight, _ = opentype.ParseCollection(notoSansSCLight)
-var scMedium, _ = opentype.ParseCollection(notoSansSCMedium)
 var scRegular, _ = opentype.ParseCollection(notoSansSCRegular)
 
 var arr = [][]font.FontFace{
 	gofont.Collection(),
-	blackItalic,
 	boldItalic,
-	lightItalic,
-	mediumItalic,
 	regularItalic,
-	black,
 	bold,
-	light,
-	medium,
 	regular,
 	notoEmoji,
 	scCollection,
 }
 
 var scCollection = []text.FontFace{
-	WithStyle(scBlack[0], font.Italic),
 	WithStyle(scBold[0], font.Italic),
-	WithStyle(scLight[0], font.Italic),
-	WithStyle(scMedium[0], font.Italic),
 	WithStyle(scRegular[0], font.Italic),
-	scBlack[0],
 	scBold[0],
-	scLight[0],
-	scMedium[0],
 	scRegular[0],
 }
 
