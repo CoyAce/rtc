6743e3707f09c60b77864f7123e2dcda61f4f1d0
CoyAce <akeycoy@gmail.com>
2026-02-26 20:22:12 +0800

    message: intercept blank text

--- ui/view/message_editor.go
+++ ui/view/message_editor.go
@@ -149,6 +149,9 @@ func (e *MessageEditor) processSubmit(gtx layout.Context) {
 	if e.Submitted(gtx) {
 		msg := strings.TrimSpace(e.InputField.Text())
 		e.InputField.Clear()
+		if msg == "" {
+			return
+		}
 		go func() {
 			message := Message{State: Stateless,
 				TextControl: NewTextControl(msg),
@@ -226,11 +229,11 @@ func (e *EditorOperator) drawBorder(gtx layout.Context, icons layout.Dimensions,
 	p := clip.Path{}
 	p.Begin(gtx.Ops)
 	p.MoveTo(f32.Point{X: minX + nw, Y: minY})
-	p.LineTo(f32.Point{X: maxX - ne, Y: minY})    // N
+	p.LineTo(f32.Point{X: maxX - ne, Y: minY}) // N
 	p.CubeTo(f32.Point{X: maxX - ne*iq, Y: minY}, // NE
 		f32.Point{X: maxX, Y: minY + ne*iq},
 		f32.Point{X: maxX, Y: minY + ne})
-	p.LineTo(f32.Point{X: maxX, Y: maxY - se})    // E
+	p.LineTo(f32.Point{X: maxX, Y: maxY - se}) // E
 	p.CubeTo(f32.Point{X: maxX, Y: maxY - se*iq}, // SE
 		f32.Point{X: maxX - se*iq, Y: maxY},
 		f32.Point{X: maxX - se, Y: maxY})
@@ -238,10 +241,10 @@ func (e *EditorOperator) drawBorder(gtx layout.Context, icons layout.Dimensions,
 	p.LineTo(f32.Point{X: midX, Y: maxY + perpendicular})       // S
 	p.LineTo(f32.Point{X: midX - triangleLegHalfSize, Y: maxY}) // S
 	p.LineTo(f32.Point{X: minX + sw, Y: maxY})                  // S
-	p.CubeTo(f32.Point{X: minX + sw*iq, Y: maxY},               // SW
+	p.CubeTo(f32.Point{X: minX + sw*iq, Y: maxY}, // SW
 		f32.Point{X: minX, Y: maxY - sw*iq},
 		f32.Point{X: minX, Y: maxY - sw})
-	p.LineTo(f32.Point{X: minX, Y: minY + nw})    // W
+	p.LineTo(f32.Point{X: minX, Y: minY + nw}) // W
 	p.CubeTo(f32.Point{X: minX, Y: minY + nw*iq}, // NW
 		f32.Point{X: minX + nw*iq, Y: minY},
 		f32.Point{X: minX + nw, Y: minY})
